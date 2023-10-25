62122b080b25b2297dc56cd85f1e05a2dd52b324
CzarProCoder <juliusczar36@gmail.com>
2023-10-25 23:07:48 +0300

    Add a class MRUCache that implements MRU caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-''' Module defining child class LRUCache
+''' Module defining child class MRUCache
 '''
 
 from collections import OrderedDict
@@ -8,37 +8,40 @@ from collections import OrderedDict
 BaseCaching = __import__('base_caching').BaseCaching
 
 
-class LRUCache(BaseCaching):
+class MRUCache(BaseCaching):
     '''Inherits from BaseCaching
     '''
     def __init__(self):
         ''' Overloads the child class
         '''
         super().__init__()
-        self.lru_order = OrderedDict()
+        self.mru_order = OrderedDict()
 
     def put(self, key, item):
         ''' Put a value item to the Cache_data dict
         based on the key
         '''
-        if key and item:
-            self.lru_order[key] = item
-            self.lru_order.move_to_end(key)
-            self.cache_data[key] = item
+        if not key or not item:
+            return
+
+        self.cache_data[key] = item
+        self.mru_order[key] = item
 
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            item_discarded = next(iter(self.lru_order))
+            item_discarded = next(iter(self.mru_order))
             del self.cache_data[item_discarded]
             print("DISCARD:", item_discarded)
 
-        if len(self.lru_order) > BaseCaching.MAX_ITEMS:
-            self.lru_order.popitem(last=False)
+        if len(self.mru_order) > BaseCaching.MAX_ITEMS:
+            self.mru_order.popitem(last=False)
+
+        self.mru_order.move_to_end(key, False)
 
     def get(self, key):
         ''' Get value from the Cache_data dict
         base on the the key
         '''
         if key in self.cache_data:
-            self.lru_order.move_to_end(key)
+            self.mru_order.move_to_end(key, False)
             return self.cache_data[key]
         return None
