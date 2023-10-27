a5f68c12538b0bcf5ea443af663156e9ca776a6e
CzarProCoder <juliusczar36@gmail.com>
2023-10-27 11:55:27 +0300

    Add a function to calculate the start index of items within a given dataset

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+'''
+'''
+
+
+def index_range(page, page_size):
+    '''
+    Returns the start index and end_index based on th
+    page and page_size of a given dataset
+
+    Args:
+        Page (int): Page to be accessed
+        Page_size (int): Size of each page within a dataset
+
+    Returns:
+        Tuple: Containig start_index and end index of a page
+
+    Examples:
+        >>> index_range(3, 10)
+        (20, 30)
+
+        >>> type(index_range(3, 10))
+        <class 'tuple'>
+
+        >>> index_range(1, 7)
+        (0, 7)
+
+        >>> index_range(3, 15)
+        (30, 45)
+    '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + (page_size)
+
+    return start_index, end_index
