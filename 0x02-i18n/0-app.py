9f506fd2e8684cbd5dc8737fb32d5c83c938fce3
CzarProCoder <juliusczar36@gmail.com>
2023-11-03 22:07:42 +0300

    Make the app file executable

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -11,6 +11,5 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html')
 
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
