702cd0c90a25abe6f97c786b7b90928aa303b4d4
CzarProCoder <juliusczar36@gmail.com>
2023-11-03 22:15:36 +0300

    Update app script and test

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -7,9 +7,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/", methods='GET', strictslashes=False)
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     '''
-    Function to render the index file from templates folder
+    Render the index file from templates folder
     '''
     return render_template('0-index.html')
