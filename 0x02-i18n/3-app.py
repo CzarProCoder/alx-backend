ed1fd7966b3b0f72fa95edf98f39ca472050b35d
CzarProCoder <juliusczar36@gmail.com>
2023-11-04 18:22:58 +0300

    Update flask app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-"""
-Parametrize templates
-"""
-
-import babel
+'''
+Entry point to the flask app
+'''
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
@@ -11,10 +9,10 @@ app = Flask(__name__)
 babel = Babel(app)
 
 
-class Config:
-    """
-    Config class
-    """
+class Config():
+    '''
+    Contain the languages
+    '''
     LANGUAGES = ['en', 'fr']
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
@@ -25,17 +23,17 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
-    """
-     determine the best match with our supported languages.
-    """
+    '''
+    Determine the best match with out selected language
+    '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
-    """
-    hello world
-    """
+    '''
+    Render the index file from templates folder
+    '''
     return render_template('3-index.html')
 
 
